c06b2f53f342cb51ee3f417641f2d8e82712c4f7
Pamellas20 <uwasepamellas@gmail.com>
2025-02-23 19:10:42 +0200

    feat: resolve vite settings

--- vite.config.js
+++ vite.config.js
@@ -10,13 +10,13 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-  },
+  // build: {
+  //   outDir: 'dist',
+  //   assetsDir: 'assets',
+  //   rollupOptions: {
+  //     output: {
+  //       manualChunks: undefined,
+  //     },
+  //   },
+  // },
 });
